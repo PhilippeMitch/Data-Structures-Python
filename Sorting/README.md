27557e167f488d6139fed9958174e4fca619547c
PhilippeMitch <philippemitch91@gmail.com>
2023-07-03 16:19:46 +0000

    Add the bubble sort algorithm

---
+++ Sorting/README.md
@@ -0,0 +1,10 @@
+## Sorting Algorithms
+
+Sorting data is needed by many modern algorithms, it is used to rearrange a given array or list of elements according to a comparison operator on the elements. The right way to sort a data depend on the type an the size of the data.
+
+Here are some of the algorithms used for sorting:
+* Bubble sort
+* Merge sort
+* Insertion sort
+* Shell sort
+* Selection sort
--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,9 +1,37 @@
-
+"""
+Date: 2023-07-03
+"""
 class Sorting:
+    """
+    This class is to experiment the different
+    sorting algorithms
+    """
+    def bubble_sort(self, list_):
+        """
+        This function receive an unorderded list
+        and return an ordered list
+        """
+        tail_idx = len(list_) - 1
+        for pass_ in range(tail_idx, 0, -1):
+            for idx in range(pass_):
+                if list_[idx] > list_[idx + 1]:
+                    list_[idx], list_[idx + 1] = list_[idx + 1], list_[idx]
+        return list
 
-    def bubble_sort():
-        pass
-
-    def bubble_sort_recursive():
-        pass
+    def bubble_sort_recursive(self, list_, flag=False):
+        """
+        This function receive an unorderded list
+        and return an ordered list
+        """
+        tail_idx = len(list_) - 1
+        while flag:
+            flag = True
+            for idx in range(tail_idx):
+                if list[idx] > list[idx + 1]:
+                    flag = False
+                    list_[idx], list_[idx + 1] = list_[idx + 1], list_[idx]
+            self.bubble_sort_recursive(list_, flag)
+        return list_
+    
+    
 
